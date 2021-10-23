dd80036aed3062ecfa0e2e668def43756fe43752
Anonav0 <commonpeople27@gmail.com>
2021-10-23 15:05:04 +0530

    work on DELETE Route

--- blog-app/app.js
+++ blog-app/app.js
@@ -89,6 +89,17 @@ app.put("/blogs/:id", (req, res) => {
     })
 });
 
+//DELETE ROUTE
+app.delete("/blogs/:id", (req,res)=> {
+    Blog.findByIdAndRemove(req.params.id, (err) => {
+        if(err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+})
+
 
 app.listen(port, () => {
     console.log('BlogApp Server started.')
